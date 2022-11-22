d49e6d790987cd3b47d7d7cb22640ef6d45505ce
Toni <Nema>
2022-11-22 08:35:38 +0100

    blitz 2 prva predaja

---
+++ Blitz 2/Zadatak 1.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+let notice = [];
+
+app.get("/SaveItem", (req, res) => {
+    var data;   
+    var temp = [];
+
+    for (i in obavjesti) {
+        data = item[i].name + " " + item[i].price + " " + item[i].brand;
+        temp.push(data);
+    }
+    data:{
+        item:{ 
+            name:"Hlace";
+            price: 70.53;
+            brand: "New"
+        }
+    }
+});
+
+/*{
+        status:"OK",
+        data:{
+            item:{
+            name:"Hlace",
+            price:70.53,
+        brand:"New"
+            }
+        }
+    }*/
+
